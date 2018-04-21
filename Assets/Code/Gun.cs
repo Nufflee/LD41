031ebdc2b4305a62e6cf319933f6cb828bf9f605
Nufflee <zifcani@gmail.com>
2018-04-21 13:51:01 +0200

    Muzzle flash and spark effect.

--- Assets/Code/Gun.cs
+++ Assets/Code/Gun.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
   public float fireRate = 0.25f;
 
   private float nextFire = -1;
+  private ParticleSystem muzzleFlash;
+  private GameObject sparkEffect;
 
   private void Start()
   {
+    muzzleFlash = transform.Find("MuzzleFlash").GetComponent<ParticleSystem>();
+    sparkEffect = Resources.Load<GameObject>("Prefabs/SparkEffect");
   }
 
   private void Update()
@@ -16,10 +21,17 @@ public class Gun : MonoBehaviour
     {
       nextFire = Time.time + fireRate;
 
+      muzzleFlash.Play();
+
       RaycastHit hit;
 
       if (Physics.Raycast(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f)), Camera.main.transform.forward, out hit))
       {
+        print(hit.normal);
+        GameObject sparkGameObject = Instantiate(sparkEffect, hit.point, Quaternion.LookRotation(-Camera.main.transform.forward));
+
+        //Destroy(sparkGameObject, 1.5f);
+
         if (hit.collider.CompareTag("Enemy"))
         {
           // Damage the enemy
