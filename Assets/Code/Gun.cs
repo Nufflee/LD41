a3442ca953091a6d25942c65e9f3a3fa9e4eec72
Nufflee <zifcani@gmail.com>
2018-04-22 13:24:19 +0200

    Recoil.

--- Assets/Code/Gun.cs
+++ Assets/Code/Gun.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -30,6 +31,15 @@ public class Gun : MonoBehaviour
   {
     if (Time.time > nextFire && magazineAmmo > 0)
     {
+      Quaternion recoilRotation = Quaternion.identity;
+
+      if (transform.localRotation.eulerAngles.z > 0.0f)
+      {
+        recoilRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, transform.localRotation.eulerAngles.z / 3));
+      }
+
+      transform.Rotate(new Vector3(0, 0, 1), 1.0f);
+
       nextFire = Time.time + fireRate;
 
       muzzleFlash.Play();
@@ -38,7 +48,7 @@ public class Gun : MonoBehaviour
 
       magazineAmmoText.text = (--magazineAmmo).ToString();
 
-      if (Physics.Raycast(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f)), Camera.main.transform.forward, out hit))
+      if (Physics.Raycast(recoilRotation * Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f)), Camera.main.transform.forward, out hit))
       {
         GameObject sparkGameObject = Instantiate(sparkEffect, hit.point, Quaternion.LookRotation(-Camera.main.transform.forward));
 
@@ -62,15 +72,25 @@ public class Gun : MonoBehaviour
       Shoot();
     }
 
-
     if (Input.GetKeyDown(KeyCode.R) && magazineAmmo < 30 && ammo > 0)
     {
       StartCoroutine(Reload());
+    }
+
+    if (transform.localRotation.eulerAngles.z > 0.0f && transform.localRotation.eulerAngles.z < 20.0f)
+    {
+      transform.Rotate(new Vector3(0, 0, 1), -0.08f);
+    }
+    else
+    {
+      transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0.0f);
     }
   }
 
   private IEnumerator Reload()
   {
+    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0.0f);
+
     isReloading = true;
 
     animation.Play();
