35a42773d9e2ae1ef8f92d2b2269ecf1f10b9082
TheChoconut <domagoj.gavranic@gmail.com>
2018-04-23 18:24:08 +0200

    Player death effect.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -10,27 +10,58 @@ public class Player : MonoBehaviour
   private RigidbodyFirstPersonController controller;
   private Gun gun;
 
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    public bool dead = false;
+    
+    private Vector3 originalPos;
+    private GameObject statisticsPanel;
+
   private void Start()
   {
     controller = GetComponent<RigidbodyFirstPersonController>();
     gun = transform.GetChild(0).GetChild(0).GetComponent<Gun>();
-
+    statisticsPanel = GameObject.Find("StatisticsPanel");
+    statisticsPanel.SetActive(false);
     StartCoroutine(Test());
   }
 
   private IEnumerator Test()
   {
     yield return new WaitForSeconds(2.0f);
+    dead = true;
     GameObject gunGameObject = gun.gameObject;
-
+    FindObjectOfType<WaveManager>().enabled = false;
     gun.enabled = false;
     controller.enabled = false;
     gunGameObject.GetComponent<Rigidbody>().isKinematic = false;
+    transform.GetChild(0).GetComponent<HeadBob>().enabled = false;
+    GetComponent<Rigidbody>().isKinematic = true;
+    transform.GetChild(0).GetComponent<UnityStandardAssets.ImageEffects.Grayscale>().enabled = true;
+    originalPos = transform.GetChild(0).localPosition;
+    controller.mouseLook.SetCursorLock(false);
+    statisticsPanel.SetActive(true);
   }
 
   // Update is called once per frame
   private void Update()
   {
+    if (dead && shakeDuration > 0)
+    {
+        transform.GetChild(0).localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+        shakeDuration -= Time.deltaTime * decreaseFactor;
+    }
+    else if(dead)
+    {
+        shakeDuration = 0f;
+        
+        // Show statistics
+    }
     if (health < 100)
     {
       health += 0.005f;
